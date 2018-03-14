08d8734b9c9db3dad7047e65a5d32881a04a3f85
Kantoki <victorcdauphine@gmail.com>
2018-03-14 17:13:17 +0100

    Add doc - Commit E1 S5

--- E1_S5/src/e1package/E1.java
+++ E1_S5/src/e1package/E1.java
@@ -6,9 +6,14 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class E1 {
+	
+	/**
+	 * allows to create our set of values
+	 * @return a set of values
+	 */
 	public static Set<Integer> setOfValues() {
 		Scanner sc = new Scanner(System.in);
-		Set<Integer> values = new LinkedHashSet<Integer>();
+		Set<Integer> values = new LinkedHashSet<Integer>(); // LinkedHashSet instead of HashSet because it doesn't sort the values
 		
 		int userValue = Integer.MAX_VALUE;
 		
@@ -20,6 +25,10 @@ public class E1 {
 		return values;
 	}
 	
+	/**
+	 * remove the elements which are equal to 0 (zero)
+	 * @param values : the set of values
+	 */
 	public static void removeZero(Set<Integer> values) {
 		Iterator<Integer> iterator = values.iterator();
 		while(iterator.hasNext()) {
@@ -29,6 +38,9 @@ public class E1 {
 			}
 		}
 	}
+	/**
+	 * Print to the user the sets of values
+	 */
 	public static void displaySets() {
 		System.out.println("Enter your values for your first set then type '0' to finish :");
 		Set<Integer> set1 = setOfValues();
@@ -40,8 +52,8 @@ public class E1 {
 		System.out.println("Your first set is : " + set1);
 		System.out.println("Your second set is : " + set2);
 		
-		Set<Integer> set3 = new LinkedHashSet<Integer>(set1);
-		set3.addAll(set2);
+		Set<Integer> set3 = new LinkedHashSet<Integer>(set1); // set3 with set1 values
+		set3.addAll(set2); // add to set3 all the elements of set2 (it doesn't add the elements which are already present)
 		
 		System.out.println("Your two sets comined are : " + set3);
 	}
